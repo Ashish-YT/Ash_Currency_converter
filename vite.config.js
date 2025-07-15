60c94cc2b81bc74752822e5ec6d14ad525416ead
Ashish~ <ashishy.yt1@gmail.com>
2025-07-15 17:40:12 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Currency_converter/',
+  base: '/Ash_Currency_converter/',
   plugins: [react()],
 })
